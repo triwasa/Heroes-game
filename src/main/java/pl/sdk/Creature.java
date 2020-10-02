ae5689694d9dd478892459fed780508ec9214132
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-10-02 13:20:19 +0200

    creture statistic

--- src/main/java/pl/sdk/Creature.java
+++ src/main/java/pl/sdk/Creature.java
@@ -1,4 +1,17 @@
 package pl.sdk;
 
 public class Creature {
+
+    private final CreatureStatistic stats;
+    private int currentHp;
+
+    Creature(){
+        stats = new CreatureStatistic("DefName",1,1,10,100);
+    }
+
+    Creature(String aName, int aAttack, int aArmor, int aMaxHp, int aMoveRange) {
+        stats = new CreatureStatistic(aName,aAttack,aArmor,aMaxHp,aMoveRange);
+    }
+
+
 }
