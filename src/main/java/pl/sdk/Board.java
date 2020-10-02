444e5160cc2179455a6b2cc2f97cbcbe47c949ed
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-10-02 10:50:12 +0200

    first test

---
+++ src/main/java/pl/sdk/Board.java
@@ -0,0 +1,13 @@
+package pl.sdk;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Board {
+
+    private final Map<Point, Creature> map;
+
+    Board() {
+        map = new HashMap<>();
+    }
+}
