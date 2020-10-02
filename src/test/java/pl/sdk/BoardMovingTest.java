564af61fc0fac3c5e54d3dd708783dd08c2808fc
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-10-02 12:10:00 +0200

    creture can move test

---
+++ src/test/java/pl/sdk/BoardMovingTest.java
@@ -0,0 +1,30 @@
+package pl.sdk;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardMovingTest {
+
+    private Board board;
+    private Creature creature;
+
+    @BeforeEach
+    void init(){
+        board = new Board();
+        creature = new Creature();
+        board.add(new Point(0,0), creature);
+    }
+
+    @Test
+    void creatureShouldMoveCorrectly(){
+        board.move(new Point(0,0), new Point(0,1));
+
+        Creature creatureFromBoard = board.get(0, 1);
+
+        assertEquals(creature,creatureFromBoard);
+        assertNull(board.get(0,0));
+    }
+
+}
