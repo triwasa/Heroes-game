f1335ed09898aa3833650a95c9345e5b83ac93f5
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-10-02 14:11:32 +0200

    attacking first test

---
+++ src/test/java/pl/sdk/AttackCreatureTest.java
@@ -0,0 +1,21 @@
+package pl.sdk;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AttackCreatureTest {
+
+    public static final int NOT_IMPORTANT = 5;
+
+    @Test
+    void creatureShouldLost10HpWhenAttackerHas20AttackAndDefenderHas10Armor(){
+        Creature attacker = new Creature("Attacker",20,NOT_IMPORTANT,100,NOT_IMPORTANT);
+        Creature defender = new Creature("Defender",NOT_IMPORTANT,10,100, NOT_IMPORTANT);
+
+        attacker.atack(defender);
+
+        assertEquals(10,defender.getCurrenthp(10));
+    }
+
+}
