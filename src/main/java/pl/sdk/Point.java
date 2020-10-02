a64043667e4b2e2ced583b5d6e7cf0e88681738f
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-10-02 10:54:26 +0200

    put and get creature implmentation

--- src/main/java/pl/sdk/Point.java
+++ src/main/java/pl/sdk/Point.java
@@ -1,5 +1,7 @@
 package pl.sdk;
 
+import java.util.Objects;
+
 class Point {
 
     final private int x;
@@ -8,5 +10,19 @@ class Point {
     Point(int aX, int aY) {
         x = aX;
         y = aY;
+    }
+
+    @Override
+    public boolean equals(Object aO) {
+        if (this == aO) return true;
+        if (aO == null || getClass() != aO.getClass()) return false;
+        Point point = (Point) aO;
+        return x == point.x &&
+                y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
