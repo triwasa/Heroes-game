85adb6cd52b16d4c4ccf5e5cf2e0b8ccceabd343
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-10-02 14:53:09 +0200

    counterAttacking test and impl

--- src/main/java/pl/sdk/Creature.java
+++ src/main/java/pl/sdk/Creature.java
@@ -4,6 +4,7 @@ public class Creature {
 
     private final CreatureStatistic stats;
     private int currentHp;
+    private boolean counterAttackedInThisTurn;
 
     Creature(){
         stats = new CreatureStatistic("DefName",1,1,10,100);
@@ -17,12 +18,23 @@ public class Creature {
 
     void attack(Creature aDefender) {
         if (isAlive()){
-            int damageToDeal = this.stats.getAttack() - aDefender.stats.getArmor();
-            if (damageToDeal < 0){
-                damageToDeal = 0;
-            }
+            int damageToDeal = calculateDamage(aDefender);
             aDefender.currentHp = aDefender.currentHp - damageToDeal;
+
+            if (!aDefender.counterAttackedInThisTurn){
+                int damageToDealInCounterAttack = aDefender.calculateDamage(this);
+                currentHp = currentHp - damageToDealInCounterAttack;
+                aDefender.counterAttackedInThisTurn = true;
+            }
         }
+    }
+
+    private int calculateDamage(Creature aDefender) {
+        int damageToDeal = this.stats.getAttack() - aDefender.stats.getArmor();
+        if (damageToDeal < 0){
+            damageToDeal = 0;
+        }
+        return damageToDeal;
     }
 
     private boolean isAlive() {
