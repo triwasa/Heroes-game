4e62efdaf852b8ac1e57a6d4a8a0c5259103399b
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-11-05 07:51:23 +0100

    implementation

--- battle-model/src/main/java/pl/sdk/Creature.java
+++ battle-model/src/main/java/pl/sdk/Creature.java
@@ -52,4 +52,8 @@ public class Creature {
     boolean canCounterAttack() {
         return !counterAttackedInThisTurn;
     }
+
+    void update() {
+        counterAttackedInThisTurn = false;
+    }
 }
