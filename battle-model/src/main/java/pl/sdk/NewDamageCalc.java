43130fd9021351e710808c9990655c5f5ea981c2
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-11-09 15:24:09 +0100

    add random to damageCalculator

--- battle-model/src/main/java/pl/sdk/NewDamageCalc.java
+++ battle-model/src/main/java/pl/sdk/NewDamageCalc.java
@@ -1,22 +1,27 @@
 package pl.sdk;
 
+import java.util.Random;
+
 public class NewDamageCalc extends DamageCalculator {
 
     @Override
     int calculateDamage(Creature aAttacker, Creature aDefender) {
+        Random rand = new Random();
+        int randValue = rand.nextInt(aAttacker.getDamage().upperEndpoint() - aAttacker.getDamage().lowerEndpoint() + 1) + aAttacker.getDamage().lowerEndpoint();
+
         double ret;
         if (aAttacker.getAttack() >= aDefender.getArmor()){
             int attackPoints = aAttacker.getAttack() - aDefender.getArmor();
             if (attackPoints > 60){
                 attackPoints = 60;
             }
-            ret = aAttacker.getDamage().lowerEndpoint() * (1 + (attackPoints)*0.05);
+            ret = randValue * (1 + (attackPoints)*0.05);
         }else{
             int defencePoints = aDefender.getArmor() - aAttacker.getAttack();
             if (defencePoints > 12){
                 defencePoints = 12;
             }
-            ret = aAttacker.getDamage().lowerEndpoint() * (1 - defencePoints *0.025);
+            ret = randValue * (1 - defencePoints *0.025);
         }
 
         if (ret < 0){
