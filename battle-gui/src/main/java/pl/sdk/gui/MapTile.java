dce5395677e4921bd251356de3fd4e5a3dfaaf47
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-11-10 15:21:30 +0100

    gui features

--- battle-gui/src/main/java/pl/sdk/gui/MapTile.java
+++ battle-gui/src/main/java/pl/sdk/gui/MapTile.java
@@ -1,30 +1,37 @@
 package pl.sdk.gui;
 
+import javafx.geometry.Pos;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
-import java.net.URL;
 
 class MapTile extends StackPane {
 
     private final Rectangle rec;
 
     public MapTile() {
-        rec = new Rectangle(40, 40, Color.WHITE);
+        rec = new Rectangle(60, 60, Color.WHITE);
         rec.setStroke(Color.BLACK);
         getChildren().add(rec);
     }
 
     void addCreature(String aName, String currentHp){
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
         ImageView image = new ImageView(new Image(getClass().getResourceAsStream("/graphics/creatures/" + aName + ".png")));
-        image.setFitHeight(40);
-        image.setFitWidth(40);
-        getChildren().add(image);
-        getChildren().add(new Text(currentHp));
+        image.setFitHeight(46);
+        image.setFitWidth(46);
+        vbox.getChildren().add(image);
+        Text text = new Text(currentHp);
+        text.setFont(new Font(10.0));
+        vbox.getChildren().add(text);
+        getChildren().add(vbox);
     }
 
     void setBackground(Color aColor){
