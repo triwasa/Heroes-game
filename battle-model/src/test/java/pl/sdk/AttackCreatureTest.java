1805e3e83fed5550a014b0985de5279d4244437f
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-11-10 11:00:36 +0100

    test for random damage

--- battle-model/src/test/java/pl/sdk/AttackCreatureTest.java
+++ battle-model/src/test/java/pl/sdk/AttackCreatureTest.java
@@ -1,8 +1,10 @@
 package pl.sdk;
 
+import com.google.common.collect.Range;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AttackCreatureTest {
 
@@ -30,11 +32,18 @@ class AttackCreatureTest {
 
     @Test
     void creatureShouldDealDamageMinus5PercentWhenArmorIsGreaterThenAttackBy2Points(){
-        Creature attacker = new Creature("Attacker",10,NOT_IMPORTANT,100,NOT_IMPORTANT,20);
+        Creature attacker = new Creature("Attacker",10,NOT_IMPORTANT,100,NOT_IMPORTANT, Range.closed(10,20), new NewDamageCalc());
         Creature defender = new Creature("Defender",NOT_IMPORTANT,12,100, NOT_IMPORTANT,NOT_IMPORTANT);
 
         attacker.attack(defender);
 
-        assertEquals(81,defender.getCurrentHp());
+//        assertEquals(81,defender.getCurrentHp());
+        for (int i = 0; i < 1000 ; i++) {
+            assertBetween(81,91,defender.getCurrentHp());
+        }
+    }
+
+    private void assertBetween(int aLow, int aHigh, int aCurrentHp) {
+        assertTrue(aCurrentHp >= aLow && aCurrentHp <= aHigh);
     }
 }
