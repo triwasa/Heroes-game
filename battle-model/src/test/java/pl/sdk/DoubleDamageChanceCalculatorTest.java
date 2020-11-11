29e425aa11603f5a9bc7edde6360c3dcb8bbf2d9
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-11-11 12:02:18 +0100

    black knight test

---
+++ battle-model/src/test/java/pl/sdk/DoubleDamageChanceCalculatorTest.java
@@ -0,0 +1,63 @@
+package pl.sdk;
+
+import com.google.common.collect.Range;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class DoubleDamageChanceCalculatorTest {
+
+    private int NOT_IMPORTANT = 5;
+    private int THE_SAME_FOR_BOTH_CREATURES = 10;
+    private Creature attacker;
+    private Creature defender;
+    private Random rand;
+
+    @BeforeEach
+    void init(){
+        attacker = new Creature.Builder()
+                .name("Double Damage Chance Test Unit")
+                .maxHp(NOT_IMPORTANT)
+                .attack(THE_SAME_FOR_BOTH_CREATURES)
+                .armor(THE_SAME_FOR_BOTH_CREATURES)
+                .damage(Range.closed(10, 20))
+                .moveRange(NOT_IMPORTANT)
+                .amount(10)
+                .build();
+        defender = new Creature.Builder()
+                .name("Defender")
+                .maxHp(NOT_IMPORTANT)
+                .attack(THE_SAME_FOR_BOTH_CREATURES)
+                .armor(THE_SAME_FOR_BOTH_CREATURES)
+                .damage(Range.closed(NOT_IMPORTANT, NOT_IMPORTANT))
+                .moveRange(NOT_IMPORTANT)
+                .amount(NOT_IMPORTANT)
+                .build();
+        rand = mock(Random.class);
+        when(rand.nextInt(anyInt())).thenReturn(0);
+    }
+
+    @Test
+    void shouldDealDoubleDamageIfRandomPositive(){
+        DamageCalculator calc = new DamageIncreaseWithRandomChanceCalculator(0.2,2.0,rand);
+
+        int result = calc.calculateDamage(attacker, defender);
+
+        assertEquals(200,result);
+    }
+
+    @Test
+    void shouldDealNormalDamageIfRandomNegative(){
+        DamageCalculator calc = new DamageIncreaseWithRandomChanceCalculator(0.2,2.0,rand);
+
+        int result = calc.calculateDamage(attacker, defender);
+
+        assertEquals(100,result);
+    }
+}
