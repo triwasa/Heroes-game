b565a1ab9b1b4030531fe7897b437833534e0016
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-11-25 08:14:29 +0100

    show first gui

---
+++ economy-gui/src/main/java/pl/sdk/gui/EconomyStart.java
@@ -0,0 +1,19 @@
+package pl.sdk.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class EconomyStart extends Application {
+
+    @Override
+    public void start(Stage aStage) throws Exception {
+        Scene scene = new Scene(FXMLLoader.load(getClass().getClassLoader().getResource("fxml/eco.fxml")));
+
+        aStage.setScene(scene);
+        aStage.setX(5);
+        aStage.setY(5);
+        aStage.show();
+    }
+}
