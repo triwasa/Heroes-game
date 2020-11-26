1435e204e8fbbc11f69de66c53f063d3ca9499eb
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-11-26 08:23:03 +0100

    fill template

---
+++ economy-gui/src/main/java/pl/sdk/gui/CreatureButton.java
@@ -0,0 +1,16 @@
+package pl.sdk.gui;
+
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import pl.sdk.creatures.Creature;
+
+
+public class CreatureButton extends Button {
+
+    public CreatureButton(EcoController aEcoController, Creature aCreature) {
+        super(aCreature.getName());
+        addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+            aEcoController.buy(aCreature);
+        });
+    }
+}
--- economy-gui/src/main/java/pl/sdk/gui/EcoController.java
+++ economy-gui/src/main/java/pl/sdk/gui/EcoController.java
@@ -1,4 +1,54 @@
 package pl.sdk.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import pl.sdk.creatures.Creature;
+import pl.sdk.creatures.NecropolisFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EcoController {
+    @FXML
+    HBox heroStateHBox;
+    @FXML
+    HBox shopsBox;
+    @FXML
+    Button readyButton;
+    private final List<Creature> creatureList;
+
+    public EcoController() {
+        creatureList = new ArrayList<>();
+    }
+
+    @FXML
+    void initialize(){
+        refreshGui();
+        readyButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> refreshGui());
+    }
+
+    private void refreshGui() {
+        shopsBox.getChildren().clear();
+        heroStateHBox.getChildren().clear();
+
+        NecropolisFactory factory = new NecropolisFactory();
+        VBox creatureShop = new VBox();
+        for (int i = 1; i < 8; i++) {
+            creatureShop.getChildren().add(new CreatureButton(this, factory.create(false,i)));
+            creatureShop.getChildren().add(new CreatureButton(this, factory.create(true,i)));
+        }
+        shopsBox.getChildren().add(creatureShop);
+
+        VBox creaturesBox = new VBox();
+        creatureList.forEach(c -> creaturesBox.getChildren().add(new Label(c.getName())));
+        heroStateHBox.getChildren().add(creaturesBox);
+    }
+
+    void buy(Creature aCreature) {
+        creatureList.add(aCreature);
+    }
 }
