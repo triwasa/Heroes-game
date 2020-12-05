7a8a4d8284a10e1a3138b1377434f920ec23460b
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-12-05 09:09:43 +0100

    add hero

---
+++ economy-model/src/test/java/pl/sdk/hero/EconomyHeroTest.java
@@ -0,0 +1,36 @@
+package pl.sdk.hero;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EconomyHeroTest {
+
+
+    private EconomyHero hero;
+
+    @BeforeEach
+    void init(){
+        hero = new EconomyHero(EconomyHero.Fraction.NECROPOLIS, 3000);
+    }
+
+    @Test
+    void shouldThrowExceptionWhileHeroHas7CreatureAndYoTryToAddNextOne(){
+        EconomyNecropolisFactory factory = new EconomyNecropolisFactory();
+        hero.addCreature(factory.create(true,1,1));
+        hero.addCreature(factory.create(true,1,1));
+        hero.addCreature(factory.create(true,1,1));
+        hero.addCreature(factory.create(true,1,1));
+        hero.addCreature(factory.create(true,1,1));
+        hero.addCreature(factory.create(true,1,1));
+        hero.addCreature(factory.create(true,1,1));
+
+        assertThrows(IllegalStateException.class, () -> hero.addCreature(factory.create(true,1,1)));
+    }
+
+    @Test
+    void shouldThrowExceptionWhileYouTrySubstractMoreGoldThanHeroHas(){
+        assertThrows(IllegalStateException.class, () -> hero.substract(3001));
+    }
+}
