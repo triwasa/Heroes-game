82166454935a348b6a99df23f4168336fa89a34f
Kraton99 <kratus1961@onet.pl>
2020-12-14 23:26:29 +0100

    Added stronghold fraction to CreatureStatistic enum

--- battle-model/src/main/java/pl/sdk/creatures/DungeonFactory.java
+++ battle-model/src/main/java/pl/sdk/creatures/DungeonFactory.java
@@ -1,7 +1,7 @@
 package pl.sdk.creatures;
 
 public class DungeonFactory {
-    
+
     public static final String EXCEPTION_MESSAGE = "We support tiers from 1 to 7";
 
     public Creature create(boolean aIsUpgraded, int aTier, int aAmount) {
