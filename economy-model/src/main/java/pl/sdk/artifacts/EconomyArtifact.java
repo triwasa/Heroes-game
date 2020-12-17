bf8d81673d6b8503ea5d968b8d132d4d0dc3b74b
PawelPruchniak <pw.pruchniak@gmail.com>
2020-12-17 14:28:40 +0100

    Created EconomyArtifact

---
+++ economy-model/src/main/java/pl/sdk/artifacts/EconomyArtifact.java
@@ -0,0 +1,18 @@
+package pl.sdk.artifacts;
+
+public class EconomyArtifact {
+
+    private final ArtifactStatistic stats;
+    private final int goldCost;
+
+
+    public EconomyArtifact(ArtifactStatistic aStats, int aGoldCost) {
+        this.stats = aStats;
+        this.goldCost = aGoldCost;
+    }
+
+    public int getGoldCost() { return goldCost; }
+
+    public String getName() { return stats.getTranslatedName(); }
+
+}
