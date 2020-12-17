beb6f80030f176c3e3a7b96f6fda93fc66f9f624
PawelPruchniak <pw.pruchniak@gmail.com>
2020-12-17 15:15:10 +0100

    Created Artifact class.

---
+++ battle-model/src/main/java/pl/sdk/artifacts/Artifact.java
@@ -0,0 +1,9 @@
+package pl.sdk.artifacts;
+
+public class Artifact {
+    private final ArtifactStatisticIf stats;
+
+    public Artifact(ArtifactStatisticIf aStats) {
+        this.stats = aStats;
+    }
+}
