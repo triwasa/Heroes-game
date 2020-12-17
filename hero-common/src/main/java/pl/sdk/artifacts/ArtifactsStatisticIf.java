4d0ee63db3ae3d11bc8bc1702b9acca5f44fa2e0
PawelPruchniak <pw.pruchniak@gmail.com>
2020-12-17 13:35:58 +0100

    interface for artifacts statistic

---
+++ hero-common/src/main/java/pl/sdk/artifacts/ArtifactsStatisticIf.java
@@ -0,0 +1,16 @@
+package pl.sdk.artifacts;
+
+public interface ArtifactsStatisticIf {
+
+    String getTranslatedName();
+
+    int getAttackSkill();
+
+    int getDefenceSkill();
+
+    int getSpellPower();
+
+    int getKnowledge();
+
+    String getRarity();
+}
