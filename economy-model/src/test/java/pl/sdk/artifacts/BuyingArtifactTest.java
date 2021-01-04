493f80d9ec40e3c2d5f48786b4b96d203b4b95d8
PawelPruchniak <pw.pruchniak@gmail.com>
2021-01-04 11:01:08 +0100

    Adding test for buying single artifact.

--- economy-model/src/test/java/pl/sdk/artifacts/BuyingArtifactTest.java
+++ economy-model/src/test/java/pl/sdk/artifacts/BuyingArtifactTest.java
@@ -1,8 +1,11 @@
 package pl.sdk.artifacts;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import pl.sdk.EconomyEngine;
 import pl.sdk.hero.EconomyHero;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BuyingArtifactTest {
 
@@ -16,5 +19,12 @@ public class BuyingArtifactTest {
         hero1 = new EconomyHero(EconomyHero.Fraction.NECROPOLIS, 1000);
         hero2 = new EconomyHero(EconomyHero.Fraction.NECROPOLIS, 1000);
         economyEngine = new EconomyEngine(hero1, hero2);
+    }
+
+    @Test
+    void heroShouldCanBuyArtifact() {
+        economyEngine.buyArtifact(artifactFactory.create("Centaur's Ax"));
+
+        assertEquals(880, hero1.getGold());
     }
 }
