032a1169d97d8448c91229925986bbbf3d656e38
Konrad <konrad1jasinski@gmail.com>
2021-01-20 15:00:11 +0100

    added Keep and KeepTest

---
+++ battle-model/src/main/java/pl/sdk/creatures/Keep.java
@@ -0,0 +1,49 @@
+package pl.sdk.creatures;
+
+import com.google.common.collect.Range;
+
+import java.util.Random;
+
+public class Keep extends Creature implements Fortification {
+
+
+    private String name = "Keep";
+    private int maxHp = 2;
+    private int currentHp = 2;
+    private int attack = 1;
+    private int level = 1;
+    public Range<Integer> damage = Range.closed(15,15);
+    private final Random rand=new Random();
+    private DefaultCalculateStrategy dealDamageCalc = new DefaultCalculateStrategy();
+
+
+    @Override
+    public int getLevel() {
+        return level;
+    }
+
+    @Override
+    public void attack(Creature aDefaultForTests){
+        int damageToDeal = dealDamageCalc.calculateDamage(this, aDefaultForTests);
+        aDefaultForTests.applyDamage(damageToDeal);
+    }
+
+    @Override
+    public Range<Integer> getDamage() {
+        return damage;
+    }
+
+    @Override
+    public int getAttack() {
+        return attack;
+    }
+
+    @Override
+    public int getAmount() {
+        if(currentHp > 0){
+            return 1;
+        }
+        else return 0;
+    }
+
+}
---
+++ battle-model/src/test/java/pl/sdk/creatures/KeepTest.java
@@ -0,0 +1,28 @@
+package pl.sdk.creatures;
+
+import com.google.common.collect.Range;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KeepTest {
+    public static final int NOT_IMPORTANT = 5;
+
+    @Test
+    void shouldShot() {
+        Keep keep = new Keep();
+        Creature defender1 = new Creature.BuilderForTesting()
+                .name("defender1")
+                .attack(10)
+                .armor(0)
+                .maxHp(100)
+                .moveRange(NOT_IMPORTANT)
+                .damage(Range.closed(20,20))
+                .build();
+        keep.attack(defender1);
+        assertEquals(85, defender1.getCurrentHp());
+
+
+    }
+
+}
