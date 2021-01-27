455e9847890312c220270d6475df411851d9cf67
Konrad <konrad1jasinski@gmail.com>
2021-01-28 00:15:06 +0100

    added WarMachineFactory

---
+++ battle-model/src/main/java/pl/sdk/creatures/WarMachineFactory.java
@@ -0,0 +1,26 @@
+package pl.sdk.creatures;
+
+public class WarMachineFactory extends AbstractFactory {
+    private static final String EXCEPTION_MESSAGE="We support tiers from 1 to 3";
+
+    @Override
+    public Creature create(boolean aIsUpgraded, int aTier, int aAmount) {
+
+
+        switch (aTier) {
+            case 1:
+                return new Catapult(CreatureStatistic.CATAPULT);
+            case 2:
+                return new FirstAidTent(CreatureStatistic.FIRSTAIDTENT);
+            case 3:
+                return new Ballista(CreatureStatistic.BALLISTA);
+            default:
+                throw new IllegalArgumentException(EXCEPTION_MESSAGE);
+        }
+
+
+    }
+
+
+}
+
