510affcb73c61e9d24fd17691579d019459147b1
TheDarkestQueen <74705079+TheDarkestQueen@users.noreply.github.com>
2021-01-28 16:15:20 +0100

    add new Attribute "name" do fields, few small changes in factory

--- battle-model/src/main/java/pl/sdk/special_fields/Field.java
+++ battle-model/src/main/java/pl/sdk/special_fields/Field.java
@@ -4,19 +4,22 @@ package pl.sdk.special_fields;
 import pl.sdk.creatures.Creature;
 
 public class Field {
+    private String fieldName;
     private boolean canStand;
-    private boolean canAttack;
+    private boolean fieldCanAttack;
     private int fieldDamage;
     private double moveRange;
 
-    public Field(boolean canStand, boolean canAttack, int damage, double moveRange) {
+    public Field(String name, boolean canStand, boolean fieldCanAttack, int damage, double moveRange) {
+        this.fieldName = name;
         this.canStand = canStand;
-        this.canAttack = canAttack;
+        this.fieldCanAttack = fieldCanAttack;
         this.fieldDamage = damage;
         this.moveRange = moveRange;
     }
 
-    public boolean getCanAttack() { return canAttack; }
+    public String getFieldName() { return fieldName; }
+    public boolean getCanAttack() { return fieldCanAttack; }
     public boolean getCanStand() { return canStand; }
     public int getFieldDamage() { return fieldDamage; }
     private double getMoveRange() { return moveRange; }
--- battle-model/src/main/java/pl/sdk/special_fields/FieldsFactory.java
+++ battle-model/src/main/java/pl/sdk/special_fields/FieldsFactory.java
@@ -7,17 +7,18 @@ public class FieldsFactory {
     public static Field create( String name ){
         switch (name) {
             case "Normal":
-                return new Field(true, false, 0, 1);
+                return new Field("Normal",true, false, 0, 1);
             case "Lava":
-                return new Field(true, true, 10, 0.4); //damage per round
+                return new Field("Lava",true, true, 10, 0.5); //damage per round, +fire magic upgrade
             case "Stone":
-                return new Field(false, false, 0, 0);
+                return new Field("Stone",false, false, 0, 0);
             case "Water":
-                return new Field(true, false, 0, 0.6);
+                return new Field("Water",true, false, 0, 0.6); //move range continue for 2 rounds, +water magic upgrade
+            case "Poison":
+                return new Field("Poison",true, true, 5, 0.8); //damage and range per round, continue for 3 rounds
             default:
                 throw new IllegalArgumentException(EXCEPTION_MESSAGE);
 
         }
     }
-
 }
