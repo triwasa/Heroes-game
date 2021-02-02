3112440632c273973c322a0095b51504e3f5c49c
PawelPruchniak <pw.pruchniak@gmail.com>
2021-02-02 20:15:43 +0100

    Added the basics of ArtifactImmunity

--- eco-battle-converter/src/main/java/pl/sdk/artifacts/ArtifactCreatureBuffFactory.java
+++ eco-battle-converter/src/main/java/pl/sdk/artifacts/ArtifactCreatureBuffFactory.java
@@ -24,6 +24,24 @@ public class ArtifactCreatureBuffFactory  extends  ArtifactAbstractFactory {
                 return new ArtifactSpeed(ArtifactStatistic.RING_OF_THE_WAYFARER, 1);
             case "Cape of Velocity":
                 return new ArtifactSpeed(ArtifactStatistic.CAPE_OF_VELOCITY, 2);
+            case "Pendant of Dispassion":
+                return new ArtifactImmunity(ArtifactStatistic.PENDANT_OF_DISPASSION, "BERZERK");
+            case "Pendant of Second Sight":
+                return new ArtifactImmunity(ArtifactStatistic.PENDANT_OF_SECOND_SIGHT, "BLIND");
+            case "Pendant of Holiness":
+                return new ArtifactImmunity(ArtifactStatistic.PENDANT_OF_HOLINESS, "CURSE");
+            case "Pendant of Life":
+                return new ArtifactImmunity(ArtifactStatistic.PENDANT_OF_LIFE, "DEATH RIPPLE");
+            case "Pendant of Death":
+                return new ArtifactImmunity(ArtifactStatistic.PENDANT_OF_DEATH, "DESTROY UNDEAD");
+            case "Pendant of Free Will":
+                return new ArtifactImmunity(ArtifactStatistic.PENDANT_OF_FREE_WILL, "HYPNOTIZE");
+            case "Pendant of Negativity":
+                return new ArtifactImmunity(ArtifactStatistic.PENDANT_OF_NEGATIVITY, "LIGHTINING");
+            case "Pendant of Total Recall":
+                return new ArtifactImmunity(ArtifactStatistic.PENDANT_OF_TOTAL_RECALL, "FORGETFULNESS");
+            case "Sphere of Permanence":
+                return new ArtifactImmunity(ArtifactStatistic.SPHERE_OF_PERMANENCE, "DISPEL");
 
             default:
                 throw new IllegalArgumentException(EXCEPTION_MESSAGE);
---
+++ eco-battle-converter/src/main/java/pl/sdk/artifacts/ArtifactImmunity.java
@@ -0,0 +1,19 @@
+package pl.sdk.artifacts;
+
+import pl.sdk.creatures.Creature;
+
+import java.util.List;
+
+public class ArtifactImmunity extends ArtifactCreatureBuff {
+    private final String spellName;
+
+    public ArtifactImmunity(ArtifactStatisticIf aStat, String aSpellName) {
+        stats = aStat;
+        this.spellName = aSpellName;
+    }
+
+    @Override
+    void buffCreature(List<Creature> creatures) {
+        creatures.forEach(creature -> creature.applyImmunity(spellName));
+    }
+}
