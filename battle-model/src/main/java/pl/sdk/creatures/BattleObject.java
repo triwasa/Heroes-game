0f84a5f2f9daa80d9f83a0351b4b23fdb4098364
Kraton99 <kratus1961@onet.pl>
2021-02-03 19:44:08 +0100

    interfaces adjustment

--- battle-model/src/main/java/pl/sdk/creatures/BattleObject.java
+++ battle-model/src/main/java/pl/sdk/creatures/BattleObject.java
@@ -3,8 +3,5 @@ package pl.sdk.creatures;
 
 
 public interface BattleObject extends Defender, Attacker {
-    //boolean canMove(Point point);
    // void moveX(Point);
-    //boolean canAttack(Point point);
-
 }
