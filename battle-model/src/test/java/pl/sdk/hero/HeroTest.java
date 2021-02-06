d42901968fb0f19864a2011bff2324c1aba00e8b
piotrwrzodak <piotrwrzodakk@gmail.com>
2021-02-06 18:46:16 +0100

    create branch from artifacts branch

--- battle-model/src/test/java/pl/sdk/hero/HeroTest.java
+++ battle-model/src/test/java/pl/sdk/hero/HeroTest.java
@@ -8,13 +8,11 @@ public class HeroTest {
 
     @Test
     void createHeroTest() {
-//        Hero hero = new Hero.BuilderForTesting().attack(1).defence(2).power(3).knowledge(4).build();
-//
-//        HeroStatistic stats = (HeroStatistic) hero.getStats();
-//
-//        assertEquals(stats.getAttack(), 1);
-//        assertEquals(stats.getDefence(), 2);
-//        assertEquals(stats.getPower(), 3);
-//        assertEquals(stats.getKnowledge(), 4);
+        Hero hero = new Hero.BuilderForTesting().attack(1).defence(2).power(3).knowledge(4).build();
+
+        assertEquals(hero.getAttack(), 1);
+        assertEquals(hero.getDefence(), 2);
+        assertEquals(hero.getPower(), 3);
+        assertEquals(hero.getKnowledge(), 4);
     }
 }
